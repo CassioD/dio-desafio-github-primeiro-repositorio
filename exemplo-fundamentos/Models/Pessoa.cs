9d436b691088763f73674b02e5c59f326aa9fffc
CassioD <cassiodans@gmail.com>
2023-03-17 17:41:05 -0300

    Adicionado programa nome e idade pessoa

--- exemplo-fundamentos/Models/Pessoa.cs
+++ exemplo-fundamentos/Models/Pessoa.cs
@@ -7,13 +7,31 @@ namespace exemplo_fundamentos.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        private string _nome;
+
+        public string Nome
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+            
+            
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+            } 
+        }
         public int Idade { get; set; }
 
         public void Apresentar()
         {
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
-
+            Console.WriteLine($"Nome: {Nome}, idade: {Idade}");
         }
 
     }
--- exemplo-fundamentos/Program.cs
+++ exemplo-fundamentos/Program.cs
@@ -1,3 +1,8 @@
 ﻿using exemplo_fundamentos.Models;
 
-Pessoa p = new Pessoa();
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Cassio";
+p1.Idade = 33;
+p1.Apresentar();
